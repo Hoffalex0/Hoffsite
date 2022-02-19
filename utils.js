e833cda71c050634d8294792601eebf8af3fd531
Hoffalex0 <simonhoffmann026@gmail.com>
2022-02-19 11:04:50 +0100

    Auslagerung von Scripts, Styling und weitere Änderungen

--- utils.js
+++ utils.js
@@ -1,3 +1,28 @@
+/*Function of the changing cursor*/
+function cursor() {
+    const note = document.querySelector('body');
+    note.style.cursor = 'url(/img/favicon.png) 0 0, wait'
+  }
+
+/*Function of the random image generator*/
+function imager() {
+    var images = ["/img/random/Winter/bahn.png", "/img/random/Winter/eichhorn.png", "/img/random/Winter/road_winter.png", "/img/random/Winter/snow_nature.png",
+    "/img/random/Winter/snowfall.png"];
+  var image = images[Math.floor(Math.random() * images.length)];
+  document.getElementById("ausgabe").innerHTML = '<img src="' + image + '">';
+}
+
+/*Function of the dark and light switch*/
+function lighter() {
+    var isDark = true;
+//Angefangen!
+}
+
+/*Function of the rain-effect*/
+function rain() {
+//Angefangen!
+}
+
 /* Load OUR (communism) navbar */
 const loadNavbar = async () => {
     const response = await fetch('/navbar.html');
